7861cb36f8a5405f07261fe0c90ddaafd09201cb
Jenna Tan <9787333+jenna215@users.noreply.github.com>
2023-08-05 12:51:59 -0400

    first try

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+const { myFetch, validateInput, formSubmission } = require("./scriptHelper");
 
-   let listedPlanets;
+
+const formSubmit = document.getElementById("formSubmit");
+const pilotName = document.getElementById("pilotName");
+const copilotName = document.querySelector("name=copilotName");
+
+
+
+
+window.addEventListener("load", function() {
+    
+    formSubmit.addEventListener("submit", function(event) {
+        let formField = document.querySelector("input[class=formField]:checked");
+        // alert the current value found in the username input
+        if (formField.value === "null") {
+            alert("All fields are required!");
+            event.preventDefault();
+        } else if (pilotName.value || copilotName.value !== "" && !formField.value.trim().match(/^[A-Z a-z 0-9\-]+$/)) {
+            alert("\nPlease enter a single keyword with only letters and/or numbers.");
+        } else {
+            validateInput();
+        }
+        event.preventDefault();
+    });
+
+    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+    formSubmission();
+    })
+
 });
+    
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,6 +1,10 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
 
+
+
+
+
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
    /*
@@ -16,15 +20,70 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
    */
 }
 
+const testInput = document.querySelector("input");
 function validateInput(testInput) {
-   
+   if (testInput === ""){
+    return "Empty";
+   } else if (isNaN(value)){
+    return "Not a Number";
+   } else {
+    return "Is a Number";
+   }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+   document = document.querySelector("body");
+   list = document.querySelector("li");
+   pilot = document.getElementById("pilotStatus");
+   copilot = document.getElementById("copilotStatus");
+   fuelLevel = document.getElementById("fuelLevel");
+   cargoLevel = document.getElementById("cargoMass");
+   launchStatus = document.getElementById("launchStatus")
+
+    pilot.querySelector("li") = (`Pilot ${pilotName} is ready for launch`);
+    copilot.querySelector("li") = (`Pilot ${copilotName} is ready for launch`);
+    if (fuelLevel < 10000){
+        document.getElementById("fuelStatus").style.display == "visible" &&
+        launchStatus.querySelector("h2") == "Shuttle not ready for launch" &&
+        launchStatus.querySelector("h2").style.color == "red"
+    } else if (cargoLevel > 10000){
+        document.getElementById("fuelStatus").style.display == "visible" &&
+        launchStatus.querySelector("h2") == "Shuttle not ready for launch" &&
+        launchStatus.querySelector("h2").style.color == "red"
+    } else {
+        launchStatus.querySelector("h2") == "Shuttle ready for launch" &&
+        launchStatus.querySelector("h2").style.color == "green"
+    };
 }
 
 async function myFetch() {
+
+    document.addEventListener('click', function (event){
+        fetch('https://handlers.education.launchcode.org/static/planets.json').then(function(response){
+            response.json().then(function(data){
+                let planets = "";
+                for(i=0; i<data.length;i++){
+                    let planet = data[i]
+                    console.log(planet.name)
+                    planets += `<div class="planet">
+                    <div id="missionTarget">
+                    <h2>Mission Destination</h2>
+                    <ol>
+                        <li>Name: ${planet.name}</li>
+                        <li>Diameter: ${planet.diameter}</li>
+                        <li>Star: ${planet.star}</li>
+                        <li>Distance from Earth: ${planet.distance}</li>
+                        <li>Number of Moons: ${planet.moons}</li>
+                    </ol>
+                    <img src="${planet.image}">
+                    </div>`
+                }
+                result.innerHTML = planets;
+            })
+        })
+    })
+
+
     let planetsReturned;
 
     planetsReturned = await fetch().then( function(response) {
